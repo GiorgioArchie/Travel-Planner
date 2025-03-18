1e728ec288e317b4a64de3b831d34149fee1d295
Jack Slater <jasl5588@colorado.edu>
2025-03-18 09:34:35 -0600

    got docker running & API Key

---
+++ ProjectSourceCode/src/resources/js/map.js
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 40.7128, lng: -74.0060 },
+    zoom: 1.5,
+  });
+}
+
+initMap();
