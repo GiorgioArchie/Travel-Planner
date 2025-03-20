0df41d17de3bb33534d47e883e11b651cd2cfe51
Arielle Lahtinen <arla1616@colorado.edu>
2025-03-20 09:30:31 -0600

    added new createSQLL and TeamMeetingLogs

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -4,21 +4,46 @@ CREATE TABLE users (
   password VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS uses_to_reviews (
+CREATE TABLE IF NOT EXISTS uses_to_trips (
   username VARCHAR(50) PRIMARY KEY NOT NULL,
-  review_id INT
+  trip_id INT
 )
 
-DROP TABLE IF EXISTS reviews CASCADE;
-CREATE TABLE IF NOT EXISTS reviews (
+CREATE TABLE IF NOT EXISTS trips (
+  trip_id INT PRIMARY KEY NOT NULL,
+  date_start date,
+  date_end date,
+  city VARCHAR(50),
+  country VARCHAR(50)
+)
+
+CREATE TABLE IF NOT EXISTS trips_to_events (
+  trip_id PRIMARY KEY NOT NULL,
+  event_id INT
+)
+
+CREATE TABLE IF NOT EXISTS events (
+  event_id INT PRIMARY KEY NOT NULL,
+  start_time time,
+  end_time time,
+  activity VARCHAR(100),
+  hotel_booking VARCHAR(100),
+  plane_tickets VARCHAR(100)
+)
+
+CREATE TABLE IF NOT EXISTS events_to_journals (
+  events_id 
+)
+
+DROP TABLE IF EXISTS journals CASCADE;
+CREATE TABLE IF NOT EXISTS journals (
   username VARCHAR(50) SERIAL PRIMARY KEY NOT NULL,
-  review_id INT,
-  review VARCHAR(500),
-  rating DECIMAL NOT NULL
+  journal_id INT,
+  comments VARCHAR(500)
 );
 
-CREATE TABLE IF NOT EXISTS review_to_image (
-  review_id PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS journal_to_image (
+  journal_id PRIMARY KEY,
   image_id INT
 );
 
