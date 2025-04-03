c8cd9a0648fb25904afcc4bdf387364a96d3dc71
Laura Zang <yuts2562@colorado.edu>
2025-04-03 10:42:23 -0600

    Awesome message.

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -97,6 +97,8 @@ const message = req.query.message || '';
 res.render('pages/register', { message, title: 'Register' });
 });
 
+// This is a comment to represent a change
+
 // Process registration form
 app.post('/register', async (req, res) => {
   try {
