bf4612f11b0efe8c1f856aa8ac1ac2c30254b15e
Laura Zang <yuts2562@colorado.edu>
2025-04-03 10:50:50 -0600

    Aweseome message.

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -69,6 +69,10 @@ app.get('/', (req, res) => {
   res.redirect('/login');
 });
 
+app.get('/new_route', (req, res) => {
+  res.redirect('/login_workdfs');
+});
+
 // Route for registration page
 app.get('/register', (req, res) => {
   const message = req.query.message || '';
